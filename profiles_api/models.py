9c5075075a774d0708a0bea377f1982a57ff51a2
thilina991 <thilinadilshan991@gmail.com>
2022-11-22 06:58:10 +0800

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,27 +5,23 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """Manager for User Profiles"""
-
-    # def __init__(self, arg):
-    #     super(UserProfileManager, self).__init__()
-    #     self.arg = arg
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """Create a User Profile"""
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
-        user.ser_password(password)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_super_user(self, email, name, password):
-        """Create a new Super User Profile with given details"""
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
@@ -33,29 +29,28 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
+        
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database mode for user in the system"""
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name =  models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FILED = 'name'
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """Retrive short name of user"""
+        """Retrieve short name of user"""
         return self.name
 
-
     def __str__(self):
-        """Return string representaion of our user"""
+        """Return string representation of user"""
         return self.email
